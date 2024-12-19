ac2a06c1c4d6941846b0496ea0814c0652bd0739
Yoniblack <166704106+Yoniblack@users.noreply.github.com>
2024-12-19 14:10:29 +0000

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,14 +1,19 @@
+
 /*
  * This should be your main class where all your objects will be created
  */
+
 package org.example;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+public class  App {
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+
+        // Creating a new book
+        Book book1 = new Book("Life of a hungry boy", " Richmond Hungry", 2000);
+
+        System.out.println(book1.title)
+
+    }
+
 }
